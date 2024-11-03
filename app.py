d7d9ad62262789c0ea3cbe51f1b9720f23cc5e81
ashwathhhhh <hackersid1199@gmail.com>
2024-11-04 01:19:24 +0530

    finished up about page

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ def login():
     else:
         return render_template("login.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 @app.route("/pages",methods = ["POST","GET"])
 def pages():
     if request.method == "POST":
